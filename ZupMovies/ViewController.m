ea3cae677b8edb0d9cd27231c10d3d59c0fdc641
Antônio Carlos <antoniocarlos.dev@gmail.com>
2016-03-09 10:58:42 -0300

    Melhorias.

--- ZupMovies/ViewController.m
+++ ZupMovies/ViewController.m
@@ -14,7 +14,7 @@
 
 @implementation ViewController
 
-NSMutableData *_responseData;
+//NSMutableData *_responseData;
 NSArray *_data;
 NSTimer *searchDelayer;
 NSString *searchTerm;
@@ -34,7 +34,7 @@ NSString *searchTerm;
 - (void) search:(NSString*)searchTerm
 {
 
-    [self searchMovieWith:searchTerm withCompletion:^(NSData *data, NSURLResponse *response, NSError *error) {
+    [self requestWithTerm:searchTerm withCompletion:^(NSData *data, NSURLResponse *response, NSError *error) {
         
         [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
         
@@ -126,39 +126,9 @@ NSString *searchTerm;
     [self search:searchBar.text];
 }
 
-#pragma mark - NSURLConnection Delegate Methods
+#pragma mark - Search
 
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    // A response has been received, this is where we initialize the instance var you created
-    // so that we can append data to it in the didReceiveData method
-    // Furthermore, this method is called each time there is a redirect so reinitializing it
-    // also serves to clear it
-    _responseData = [[NSMutableData alloc] init];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    // Append the new data to the instance variable you declared
-    [_responseData appendData:data];
-}
-
-- (NSCachedURLResponse *)connection:(NSURLConnection *)connection
-                  willCacheResponse:(NSCachedURLResponse*)cachedResponse {
-    // Return nil to indicate not necessary to store a cached response for this connection
-    return nil;
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    // The request is complete and data has been received
-    // You can parse the stuff in your instance variable now
-    
-}
-
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    // The request has failed for some reason!
-    // Check the error var
-}
-
-- (void) searchMovieWith:(NSString*)searchTerm withCompletion:(void (^)(NSData *data, NSURLResponse *response, NSError *error)) handler
+- (void) requestWithTerm:(NSString*)searchTerm withCompletion:(void (^)(NSData *data, NSURLResponse *response, NSError *error)) handler
 {
     
     NSString *url = [NSString stringWithFormat:@"%@%@%@%@", SERVER_PATH, @"?s=", searchTerm, @"&page=1"];
